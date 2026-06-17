ba1762c80a15717efea090e036d8ddb71764e71d
bebroid <lukabyckov@gmail.com>
2026-06-17 08:46:19 +0800

    gallery-Api

---
+++ src/main.js
@@ -0,0 +1,46 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api';
+
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const query = e.target.elements['search-text'].value.trim();
+  e.target.reset();
+
+  if (!query) return;
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+    hideLoader();
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+        backgroundColor: '#EF4040',
+        messageColor: '#fff',
+        messageSize: '16px',
+        maxWidth: 432,
+      });
+      return;
+    }
+
+    createGallery(data.hits);
+  } catch (error) {
+    hideLoader();
+  }
+});
